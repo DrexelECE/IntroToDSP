43cad4e6aa4873ce89eea8a68dbee2226471ada2
James K <jkrrv@jkrrv.org>
2016-01-15 15:11:19 -0500

    Task 3 Complete

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -1,5 +1,5 @@
 %% Task 1
-clc;
+clear; clc;
 
 x = 2+3i;
 y = 5*exp(-1i*3/5);
@@ -14,9 +14,10 @@ printAmpPhase(v1);
 
 
 %% Task 2
-clc;
+clear; clc;
 
-x1 = -1:0.05:1;
+x1 = -1:0.01:1;
+x2 = -1:0.20:1;
 
 figure(1);
 plot(x1,u(x1), x2, u(x2));
@@ -33,6 +34,9 @@ fprintf(['Steps of 0.01 better represent the ideal step function than steps ' ..
         'immediatly before the first 1 value.']);
     
 
+x1 = -1:0.05:1;
+    
+    
 figure(2);
 plot(x1,r(x1),'-ob', ...
     'MarkerEdgeColor', 'r', ...
@@ -43,8 +47,23 @@ xlabel('x');
 ylabel('r(x)');
 grid on;
 
+
 %% Task 3
+clear; clc;
+t = -2:0.001:4;
+
+f1 = 4 + 2*u(t+3) - r(t+1) + r(t-5);
+f2 = u(t) .* u(6-t); % Assuming use of the Hadamard product
+f3 = u(t) - u(6-t);
+f4 = 2*sin(r(t));
+f5 = r(2*sin(t));
 
 
-
+figure(3);
+plot(t, f1, t, f2, t, f3, t, f4, t, f5);
+grid on;
+title 'Plots of given functions';
+xlabel 't';
+ylabel 'f(t)';
+legend('f1(t)','f2(t)','f3(t)','f4(t)','f5(t)');
 
